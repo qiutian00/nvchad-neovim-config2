ddc183e01b878f86a187cb22884421ae2c0a6f5a
qiutian00 <1476102147@qq.com>
2026-09-13 11:49:25 +0800

    fix: 配置biom来格式化

--- lua/nvchad/configs/conform.lua
+++ lua/nvchad/configs/conform.lua
@@ -1,13 +1,31 @@
+local prettier_markers = {
+  ".prettierrc",
+  ".prettierrc.json",
+  ".prettierrc.js",
+  ".prettierrc.cjs",
+  ".prettierrc.mjs",
+  ".prettierrc.yaml",
+  ".prettierrc.yml",
+  ".prettierrc.toml",
+  "prettier.config.js",
+  "prettier.config.cjs",
+  "prettier.config.mjs",
+}
+
+local function formatter_for(bufnr)
+  return vim.fs.root(bufnr, prettier_markers) and { "prettier" } or { "biome" }
+end
+
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-	javascript = { "prettier" },
-	javascriptreact = { "prettier" },
-    typescript = { "prettier" },
-    typescriptreact = { "prettier" },
-    json = { "prettier" }, -- 如果也需要格式化 JSON
+    css = formatter_for,
+    html = formatter_for,
+    javascript = formatter_for,
+    javascriptreact = formatter_for,
+    typescript = formatter_for,
+    typescriptreact = formatter_for,
+    json = formatter_for,
   },
 
   -- format_on_save = {
