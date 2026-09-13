98bd9cce83f9e23a226f0b3ac6fe7802cfdaa08d
qiutian00 <1476102147@qq.com>
2026-09-13 10:27:36 +0800

    fix: mac os nvchad  nvim source init

---
+++ init.lua
@@ -0,0 +1,79 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = {
+  defaults = { lazy = true },
+  install = { colorscheme = { "nvchad" } },
+  ui = {
+    icons = {
+      ft = "",
+      lazy = "󰂠 ",
+      loaded = "●",
+      not_loaded = "○",
+    },
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+      },
+    },
+  },
+}
+
+-- load plugins
+require("lazy").setup({
+  {
+    -- 使用本地 NvChad 仓库（即当前 ~/.config/nvim）
+    dir = vim.fn.stdpath "config",
+    lazy = false,
+    import = "nvchad.plugins",
+  },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "nvchad.mappings"
+end)
